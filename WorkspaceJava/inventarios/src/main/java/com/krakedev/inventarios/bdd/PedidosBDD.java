fadb6028623bc56967feedea092b32b11ef4bf7a
JoseDutan <josedutan86@gmail.com>
2024-04-30 15:09:40 -0500

    RETO 14 COMPLETADO

--- WorkspaceJava/inventarios/src/main/java/com/krakedev/inventarios/bdd/PedidosBDD.java
+++ WorkspaceJava/inventarios/src/main/java/com/krakedev/inventarios/bdd/PedidosBDD.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -71,8 +72,13 @@ public class PedidosBDD {
 		Connection con = null;
 		PreparedStatement ps = null;
 		PreparedStatement psDet = null;
+		PreparedStatement psHis = null;
+		
+		Date fechaActual = new Date();
+		Timestamp fechaHoraActual = new Timestamp(fechaActual.getTime());
 		try {
 			con = ConexionBDD.obtenerConexion();
+			
 			ps = con.prepareStatement("UPDATE cabecera_pedido SET estado = ? WHERE numero = ?");
 			ps.setString(1, "R");
 			ps.setInt(2, pedido.getNumero());
@@ -93,6 +99,15 @@ public class PedidosBDD {
 				psDet.setInt(3, det.getCodigo());
 				
 				psDet.executeUpdate();
+				
+			 psHis = con.prepareStatement("insert into historial_stock(fecha, referencia, producto, cantidad) "
+			 		+ "values(?, ?, ?, ?)");
+			 psHis.setTimestamp(1, fechaHoraActual);
+			 psHis.setString(2, "PEDIDO "+pedido.getNumero());
+			 psHis.setInt(3, det.getProducto().getCodigo());
+			 psHis.setInt(4, det.getCantidadRecibida());
+			 
+			 psHis.executeUpdate();
 			}
 
 		} catch (KrakeDevException e) {
